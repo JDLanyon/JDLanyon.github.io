62b38596f25cb745807a06e42f070f0756f1e386
JDLanyon <jlany9@hotmail.com>
2025-03-12 16:01:43 +1000

    new nextjs app go brr

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,10 @@
+import "../globals.css";
+
+export default function Hero() {
+  return (
+    <div id="hero">
+      <h1 className="!font-[family-name:var(--font-doto)]">Home of epic B)</h1>
+      <a>Thanks for visiting my portfolio, get started with the options below!</a>    
+    </div>
+  )
+}
