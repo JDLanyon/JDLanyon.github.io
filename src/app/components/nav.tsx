e63b8d1c93522a588526bab078a4996455babe77
JDLanyon <jlany9@hotmail.com>
2025-04-05 08:17:10 +1000

    navbar
    still need to make burger menu work

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -2,11 +2,67 @@
 
 import "../globals.css";
 import Image from "next/image";
-
+import { Menu } from 'lucide-react';
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className="grid grid-cols-7 w-screen gap-8 items-center justify-center text-center fixed top-0">
+      {/* left side */}
+      <div className="col-start-2 col-span-2 mx-auto gap-16 hidden md:relative md:flex">
+        <a
+          className="flex items-center hover:underline hover:underline-offset-4 text-[var(--secondary)]"
+          href="/"
+          rel="noopener noreferrer"
+        >
+          Home
+        </a>
+        <a
+          className="flex items-center hover:underline hover:underline-offset-4 text-[var(--secondary)]"
+          href="/about"
+          rel="noopener noreferrer"
+        >
+          About
+        </a>
+      </div>
+
+      {/* Lotus */}
+      <a className="col-start-4 mx-auto col-300" href="/">
+        <Image
+          className="dark:invert"
+          src="/lotus.svg"
+          width={60}
+          height={60}
+          alt="lotus"
+        />
+      </a>
+
+      {/* right side */}
+      <div className="col-start-5 col-span-2 mx-auto gap-16 hidden md:relative md:flex">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-[var(--secondary)]"
+          href="/"
+          rel="noopener noreferrer"
+        >Programming</a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-[var(--secondary)]"
+          href="/"
+          rel="noopener noreferrer"
+        >Motion Graphics</a>
+      </div>
+      
+
+      {/* mobile options */}
+
+      {/* burger menu */}
+      <div className="col-start-7 mx-auto gap-16 md:hidden">
+        <button onClick={async () => {
+          console.log("hi");
+        }}>
+        <Menu color="var(--secondary)" />
+
+        </button>
+      </div>
+
 
     </nav>
   )
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Hero from "../app/components/hero";
 import Footer from "../app/components/footer";
 import Button from "../app/components/button";
+import Nav from "../app/components/nav";
 
 export default function About() {
   return (
     <div>
+      <Nav />
       <div className="grid items-center justify-items-center min-h-screen p-8 gap-16">
       <div className="row-start-1 items-center text-center">
             <p>who the hell is this "Grey Ming" guy anyways?</p>
