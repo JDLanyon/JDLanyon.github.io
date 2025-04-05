762731b3973cad54c46c557269fe31aac37fa113
JDLanyon <jlany9@hotmail.com>
2025-04-05 19:49:41 +1000

    landing page background

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -2,7 +2,7 @@ import "../globals.css";
 
 export default function Hero() {
   return (
-    <div id="hero">
+    <div id="hero" className="z-1">
       <h1 className="!font-[family-name:var(--font-doto)]">Home of epic B)</h1>
       <a>Thanks for visiting my portfolio, start exploring with the options below!</a>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,22 @@ import Button from "./components/button";
 export default function Home() {
   return (
     <div>
-      <div className="grid items-center justify-items-center h-screen p-8 gap-16">
+      <img
+          className="hidden lg:block w-full h-full bg-cover absolute z-0 light:invert"
+          src="/BG_1920.gif"
+          width={1920}
+          height={1080}
+          alt="lotus"
+        />
+        {/* mobile BG */}
+        <img
+          className="block lg:hidden w-full h-full bg-cover absolute z-0 light:invert"
+          src="/BG_768.gif"
+          width={1920}
+          height={1080}
+          alt="lotus"
+        />
+      <div className="grid items-center justify-items-center h-screen p-8 gap-16 z-1">
         <main className="flex flex-col gap-8 items-center text-center">
           <Hero />
           <div className="pt-16 flex gap-4 items-center flex-col md:flex-row">
